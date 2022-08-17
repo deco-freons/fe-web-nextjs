75c2659692f5930ef9d75e8640289b59fd9f2415
Muhammad Rafadana Mountheira <RafadanaM@gmail.com>
2022-08-17 20:31:13 +1000

    refactor: add with credentials

--- network/apiClient.ts
+++ network/apiClient.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-    
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default apiClient;
